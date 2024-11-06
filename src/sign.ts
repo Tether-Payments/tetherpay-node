735b095ca888a3d0a0d9df050c0d61496e86ffb4
Mitch Skomra <mitchskomra@gmail.com>
2024-11-05 22:36:07 -0500

    update signing methods
    - Updated the signing and verify methods to expect the same params as server

--- src/sign.ts
+++ src/sign.ts
@@ -1,44 +1,29 @@
 import { createSign, createVerify } from 'crypto';
 
 export type SignedRequestParams = {
-  method: 'GET' | 'POST' | 'PUT' | 'PATCH' | 'DELETE' | string;
-  uri: string;
-  timestamp: number;
   body: string | undefined | null;
 };
 
 export function generateSignature(
   privateKey,
-  { method, uri, timestamp, body }: SignedRequestParams
+  { body = '{}' }: SignedRequestParams
 ) {
-  const url = new URL(uri);
-  const path = `${url.pathname}${url.search ? url.search : ''}`;
-
   const sign = createSign('SHA256');
 
-  sign.update(`${method.toUpperCase()}${path}${timestamp}`);
-  if (body) {
-    sign.update(Buffer.from(JSON.stringify(body)));
-  }
+  sign.update(Buffer.from(JSON.stringify(body)));
 
   sign.end();
-  return Buffer.from(sign.sign(privateKey)).toString('base64');
+  return sign.sign(privateKey, 'base64');
 }
 
 export function verifySignature(
   publicKey,
   signature,
-  { method, uri, timestamp, body }: SignedRequestParams
+  { body = '{}' }: SignedRequestParams
 ) {
-  const url = new URL(uri);
-  const path = url.pathname + url.search;
-
   const verify = createVerify('SHA256');
 
-  verify.update(`${method.toUpperCase()}${path}${timestamp}`);
-  if (body) {
-    verify.update(Buffer.from(JSON.stringify(body)));
-  }
+  verify.update(Buffer.from(JSON.stringify(body)));
 
   verify.end();
   return verify.verify(publicKey, Buffer.from(signature, 'base64'));
