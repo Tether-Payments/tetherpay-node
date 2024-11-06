5394cb2c283075b92f52c67da3f1ddf0bc9df02e
Mitch Skomra <mitchskomra@gmail.com>
2024-11-05 22:39:32 -0500

    Update example.ts

--- example/example.ts
+++ example/example.ts
@@ -16,33 +16,39 @@ const tpgClient = new TetherPayments({
 
 (async () => {
   const server = createServer((req, res) => {
-    const host = req.headers['host'];
     const signature = req.headers['x-signature'];
-    const timestamp = req.headers['x-timestamp'];
-    const { method, url } = req;
 
-    const params = {
-      method: method!,
-      uri: `http://${host}${url}`,
-      timestamp: parseInt(timestamp?.toString()!),
-      body: null,
-    };
+    let body = [];
+    req.on('data', chunk => body.push(chunk as never));
 
-    const validSig = verifySignature(keyPair.publicKey, signature?.toString(), params);
-    console.log('valid signature?', validSig);
+    req.on('end', () => {
+        let parsedBody;
+        if (req.headers['content-type'] === 'application/json') {
+            parsedBody = JSON.parse(Buffer.concat(body).toString());
+        }
 
-    res.statusCode = 200;
-    res.setHeader('Content-Type', 'application/json');
-    res.end(JSON.stringify({ ok: 1 }));
+        const params = {
+          body: parsedBody ? JSON.stringify(parsedBody) : undefined,
+        };
+    
+        const validSig = verifySignature(keyPair.publicKey, signature?.toString(), params);
+        console.log('valid signature?', validSig);
+    
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(JSON.stringify({ ok: 1 }));
+    });
   });
 
-  server.listen(port, hostname, async () => {
-    console.log(`Server running at http://${hostname}:${port}/`);
+  await server.listen(port, hostname);
+  console.log(`Server running at http://${hostname}:${port}/`);
 
     // Call SDK method
-    const res = await tpgClient.getSessionById(1);
+    const res = await tpgClient.createSession({
+      toUUID: '1ab6d1b9-9a0f-4057-96ce-b8f3e057fcd5',
+      referenceNumber: '1',
+    });
     console.log(res);
 
     process.exit();
-  });
 })();
